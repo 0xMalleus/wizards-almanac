2dd6d3e0285f600d8c1dedcf610ab3674f79f907
0xMalleus <96664948+0xMalleus@users.noreply.github.com>
2022-02-06 13:11:42 -0600

    fix(app-controller): Updates welcome text

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getHello(): string {
-    return 'Hello Friend. Put thy Rune on the Door and checkout /docs for more info.';
+    return "Φ Ψ λ ϐ ҩ Ӝ ⊙ ⚧ ⚭ Ω Welcome to the Wizards' Almanac Φ Ψ λ ϐ ҩ Ӝ ⊙ ⚧ ⚭ Ω -- checkout /docs for more info.";
   }
 }
